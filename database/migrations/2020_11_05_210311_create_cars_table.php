550e2217122bba3c5a1a94d220021f3e5cdcf0b8
NazarSm <69159497+NazarSm@users.noreply.github.com>
2020-11-05 23:18:33 +0200

    added migration

---
+++ database/migrations/2020_11_05_210311_create_cars_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('brand');
+            $table->string('model');
+            $table->string('name');
+            $table->integer('phoneNumber');
+            $table->string('email')->unique();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
